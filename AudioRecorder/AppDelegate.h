882ed8c6d5c992a85f4c9b04b016e1e8a709746e
Kevin Kwon <matoson@gmail.com>
2014-06-30 11:59:25 +0900

    Initial Commit

---
+++ AudioRecorder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioRecorder
+//
+//  Created by hdk on 2014. 6. 30..
+//  Copyright (c) 2014년 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioRecorderTests/AudioRecorderTests.m
@@ -0,0 +1,34 @@
+//
+//  AudioRecorderTests.m
+//  AudioRecorderTests
+//
+//  Created by hdk on 2014. 6. 30..
+//  Copyright (c) 2014년 Kevin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioRecorderTests : XCTestCase
+
+@end
+
+@implementation AudioRecorderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
