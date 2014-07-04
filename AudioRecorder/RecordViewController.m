cc5c6da4810184443986bd5224ecae1e82e70dec
Kevin Kwon <matoson@gmail.com>
2014-07-04 18:01:02 +0900

    RecordViewController 코딩중

--- AudioRecorder/RecordViewController.m
+++ AudioRecorder/RecordViewController.m
@@ -27,12 +27,33 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+
+    [self SetAudioSession]; // 오디오 동작 설명
+    [recordTimeDisplay setFont:[UIFont fontWithName:@"DBLCDTempBlack" size:40.0]]; // 폰트 설정
+    pDateBase = [[RecordDataBase alloc] init]; // 디비 초기화
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 추가된 함수
+- (BOOL)SetAudioSession
+{
+    self.pAudioSession = [AVAudioSession sharedInstance];
+    
+    // 오디오 카테고리를 설정합니다.
+    if (![self.pAudioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil]) {
+        return NO;
+    }
+    // 오디오 세션이 활성화 됩니다.
+    if (![self.pAudioSession setActive:YES error:nil]) {
+        return NO;
+    }
+    
+    return self.pAudioSession.inputIsAvailable;
 }
 
 @end
